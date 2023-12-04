84118f8d92cf58bb1322a6a8449e4b962bf3adfa
luccabibar <luccabibar@gmail.com>
2023-12-04 02:28:22 -0300

    just the two of them

---
+++ Assets/Scripts/botControl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botControl : playerBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // deve implementar
+    } 
+}
